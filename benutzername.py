ecd8846f9827abd0c3659132d70a5db3c64327a5
liviazimmerli <97448614+liviazimmerli@users.noreply.github.com>
2022-01-10 13:53:57 +0100

    Update benutzername.py

--- benutzername.py
+++ benutzername.py
@@ -1,10 +1,15 @@
 benutzername = input ("Benutzername eingeben")
+#Das Programm überprüft den eingegebenen Benutzername.
 if benutzername == ("livia"):
+    #Wenn der Benutzername korrekt eigegeben wurde, wird nach dem Passwort gefragt.
     password= input("Passwort eingeben")
+    #Wenn das Passwort korrekt eigegeben wurde, wird das Login genehmigt.
     if password == "gutentag":
         print ("Passwort genehmigt")
+        #Wenn das Passwort falsch war, hat man weitere Versuche um sich einzuloggen.
     else: 
         print ("passwort falsch")
+# Wenn der Benutzername schon falsch eingegeben wird, läuft das Programm nicht weiter.
 else:
     print("Benutzername falsch")
 
