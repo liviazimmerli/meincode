7916af6644b67d681326075d197c5bc94de4b953
liviazimmerli <97448614+liviazimmerli@users.noreply.github.com>
2022-01-10 13:48:51 +0100

    Add files via upload

---
+++ benutzername.py
@@ -0,0 +1,11 @@
+benutzername = input ("Benutzername eingeben")
+if benutzername == ("livia"):
+    password= input("Passwort eingeben")
+    if password == "gutentag":
+        print ("Passwort genehmigt")
+    else: 
+        print ("passwort falsch")
+else:
+    print("Benutzername falsch")
+
+
